44f08f0ad9457fedeef311fd10b074b4d65bd0ae
poongeta <96272766+poongeta@users.noreply.github.com>
2026-02-15 17:17:13 +0700

    spawn player in map

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PlayerData
+{
+    public string playerID;
+    public bool isReady;
+    public WeaponType weaponType;
+}
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance;
+
+    public PlayerData[] players = new PlayerData[4];
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            for (int i = 0; i < players.Length; i++)
+                players[i] = new PlayerData();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetPlayerData(string id, bool ready, WeaponType weapon)
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].playerID == id || string.IsNullOrEmpty(players[i].playerID))
+            {
+                players[i].playerID = id;
+                players[i].isReady = ready;
+                players[i].weaponType = weapon;
+
+                PrintAllPlayers(); 
+                return;
+            }
+        }
+
+        PrintAllPlayers(); 
+    }
+    //for debug
+    private void PrintAllPlayers()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i] == null) continue;
+
+            Debug.Log(
+                $"Slot {i} | ID: {players[i].playerID} | Ready: {players[i].isReady} | Weapon: {players[i].weaponType}"
+            );
+        }
+    }
+
+
+    public void RemovePlayer(string id)
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].playerID == id)
+            {
+                players[i] = new PlayerData();
+                return;
+            }
+        }
+    }
+
+    public int ReadyCount()
+    {
+        int count = 0;
+        foreach (var p in players)
+            if (p != null && p.isReady)
+                count++;
+
+        return count;
+    }
+}
