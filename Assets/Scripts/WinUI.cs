4a59efc9cef056185b2a7287bda45c20ca0c094b
poongeta <96272766+poongeta@users.noreply.github.com>
2026-02-17 10:51:21 +0700

    ScoreBoardText

--- Assets/Scripts/WinUI.cs
+++ Assets/Scripts/WinUI.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 public class UIManager : MonoBehaviour
 {
@@ -6,6 +7,7 @@ public class UIManager : MonoBehaviour
 
     public GameObject roundEndPanel;
     public TMPro.TextMeshProUGUI winnerText;
+    public TMPro.TextMeshProUGUI scoreBoard;
 
     void Awake()
     {
@@ -18,6 +20,8 @@ public class UIManager : MonoBehaviour
         Debug.Log("ShowingRoundEnd");
         roundEndPanel.SetActive(true);
         winnerText.text = winner.name + "'s Victory!";
-       
+        scoreBoard.text = string.Join("\n", scores
+            .OrderByDescending(kvp => kvp.Value)
+            .Select(kvp => $"{kvp.Key}: {kvp.Value}"));
     }
 }
